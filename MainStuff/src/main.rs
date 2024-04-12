2e8eaf996875f41e536d4ff806fe23ab8a087589
Monsieur-Gray <166855992+Monsieur-Gray@users.noreply.github.com>
2024-04-12 18:07:17 +0000

    Create main.rs

---
+++ MainStuff/src/main.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+#![allow(unused)]
+
+use my_modules::*;
+use my_modules::defkeys::Builtins;
+
+use builtin_fns::EXECUTE::check_exec_line;
+use builtin_fns::COMPILE::compile_to_cpp;
+use builtin_fns::compile_ass::compile_to_asm;
+
+fn main() {
+    let text = read_file::read_from_str("f2.caxy");
+
+    let CODE: Vec<Vec<Builtins>> = get_type::get_type(text);
+
+    let (_msec, _vsec) = split_sec::split_code(&CODE);
+    
+    let (mut STACK, mut HEAP) = mem_alloc::calloc(&_vsec);
+        
+// //--=-=-=-=-==-=-=-==============------------------------------------=-=-=-=-=-==-=-------------------------------
+    println!("<================-================>");
+    // check_exec_line(&_msec, STACK, HEAP);
+    compile_to_cpp(&_msec, STACK, HEAP);
+    // compile_to_asm(&_msec, STACK, HEAP);
+    println!("<================-================>");
+//--=-=-=-=-==-=-=-==============------------------------------------=-=-=-=-=-==-=-------------------------------
+
+}
