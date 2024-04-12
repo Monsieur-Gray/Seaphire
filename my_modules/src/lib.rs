d10a1b30dee8945c61fcfb271d4fea033a8ef37c
Monsieur-Gray <166855992+Monsieur-Gray@users.noreply.github.com>
2024-04-12 18:12:53 +0000

    Create lib.rs

---
+++ my_modules/src/lib.rs
@@ -0,0 +1,57 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+pub mod defkeys;
+
+pub mod read_file;
+pub mod get_type;
+pub mod split_sec;
+pub mod fetch_data; // also available in builtin_fns
+
+pub mod mem_alloc;
+
+
+#[macro_export]
+macro_rules! Throw {
+    ($msg:literal) => {{        // &str
+        use colored::*;
+        panic!(
+            "{}", format!("->!> {}", $msg)
+            .red().bold()
+            .on_truecolor(15, 15, 15)
+        )
+    }};
+
+    ($msg:expr) => {{          // String
+        use colored::*;
+        panic!(
+            "{}", format!("->!> {}", $msg)
+            .red().bold()
+            .on_truecolor(15, 15, 15)
+        )
+    }};
+}
+
+
+#[macro_export]
+macro_rules! SysThrow {
+    ($msg:literal) => {{        // &str
+        use colored::*;
+        panic!(
+            "{}", format!("->!> {}", $msg)
+            .truecolor(250, 150, 125)
+            .bold()
+            .on_truecolor(25, 25, 25)
+        )
+    }};
+
+    ($msg:expr) => {{          // String
+        use colored::*;
+        panic!(
+            "{}", format!("->!> {}", $msg)
+            .truecolor(200, 150, 100)
+            .bold()
+            .on_truecolor(20, 20, 20)
+        )
+    }};
+}
