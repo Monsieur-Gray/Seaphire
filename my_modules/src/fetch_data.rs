c00a67ba50274d39b378a08170207486f565261b
Monsieur-Gray <166855992+Monsieur-Gray@users.noreply.github.com>
2024-04-25 08:27:02 +0000

    Add files via upload

--- my_modules/src/fetch_data.rs
+++ my_modules/src/fetch_data.rs
@@ -10,8 +10,8 @@ pub fn fetch_num(data: &Builtins) -> Result<f32, &str> {
 
 pub fn fetch_str(data: &Builtins) -> Result<String, &str> {
     match data {
-        Builtins::D_type(D_type::str( d )) => Ok(d.to_string()),
-        Builtins::ID(d) => Ok(d.to_string()),
+        Builtins::D_type(D_type::str( d )) => Ok(chk_qmark(d)),
+        Builtins::ID(d) => Ok(chk_qmark(d)),
         _ => Err("fetch_str ::> Fetch Error!")
     }
 }
@@ -31,11 +31,48 @@ pub fn fetch_Univ(data: &Builtins) -> Result<f32, String> {
         
         Builtins::D_type(D_type::str( d )) => Err(chk_qmark(d)),
         Builtins::ID(d) => Err(chk_qmark(d)),
-        _ => crate::Throw!("fetchUNIV cant do shit")
+        _ => crate::Throw!("fetch-UNIV cant do shit")
     }
 }
 /*================================================================================ */
 /*================================================================================ */
+pub fn fetch_MyRes(data: &Builtins) -> MyRes<i32, f32, String, bool> {
+    match data {
+        Builtins::D_type(D_type::int(s_int)) => MyRes::Int(*s_int),
+        Builtins::D_type(D_type::float(s_flt)) => MyRes::Flt(*s_flt),
+
+        Builtins::D_type(D_type::str( d )) => MyRes::Str(chk_qmark(d)),
+        Builtins::ID(d) => MyRes::Str(chk_qmark(d)),
+
+        Builtins::D_type(D_type::bool(b)) => MyRes::Bool(*b),
+        _ => crate::Throw!("I cant do shit")
+    }
+}
+
+pub fn get_val(var: &Builtins,
+    stack_hash: &std::collections::HashMap<String, Builtins>,
+    heap_hash: &std::collections::HashMap<String, Builtins> ) -> Option<Builtins>
+{
+    let a = match var {
+        Builtins::D_type(_) => var,
+        Builtins::ID(id) => {
+            if let Some(v) = stack_hash.get( id ) { v }
+            else if let Some(v) = heap_hash.get( id ){ v }
+            else { return None; }
+        },
+        _ => crate::Throw!("What in actual fuck is this")
+    };
+    
+    return Some(a.clone());
+}
+
+#[derive(Debug)]
+pub enum MyRes<I, F, S, B>{
+    Int(I),
+    Flt(F), 
+    Str(S),
+    Bool(B)
+}
 
 fn chk_qmark(s: &String) -> String {
     if s.starts_with('?') {
