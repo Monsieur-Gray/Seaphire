fc85eadb469fb1dc130a5900d931edb890149041
Ishaan Garud <166855992+Monsieur-Gray@users.noreply.github.com>
2026-05-11 02:00:47 +0530

    Updated Memory Structure !
    Updated the entire bloody memory structure to a more professional setup. More scalable, optimized (relatively), faster, better

--- main_project/src/main.rs
+++ main_project/src/main.rs
@@ -3,15 +3,18 @@
 
 use colored::Colorize;
 use SysBase::{SysThrow, PARSER};
+use SysBase::memory_layout::{Memory, Env, MemCell};
 
 // use pipeline::CppCompiler::*;
 use core_utils::EXECUTE_2::check_exec_line;
+
+// ! 7:05pm
 
 fn main() {
     use std::fs::read_to_string;
 
     // ENTER YOUR FILE NAME HERE ->
-    let file_name = "src/debug.inu";
+    let file_name = "src/calc2.inu";
     let txt = if let Ok(bruh) = read_to_string(&file_name) {
         bruh
     } else {
@@ -23,9 +26,25 @@ fn main() {
     // println!("heh ==> {:?}", &mvec);
     let MSEC = PARSER::make_msec(mvec);
     // println!("heh ==> {:?}", MSEC);
-    let [sh, hh, regh] = PARSER::calloc(vvec);
+
+    let vars = PARSER::collect_vars(vvec);
+    let mut memory =  &mut Memory::new();
+    let mut env = &mut Env::new();
+
+    for var in vars {
+        let memcell: MemCell = MemCell {
+            data: var.data,
+            scope: var.scope,
+            is_mutable: var.is_mutable
+        };
+
+        let ptr: usize = memory.alloc(memcell);
+
+        env.insert(var.name, ptr);
+    }
+
 
     println!("\nCURRENTLY RUNNING ->  {}\n", file_name.purple().bold());
     // compile_to_cpp(&MSEC, sh, hh);
-    check_exec_line(&MSEC, sh, hh, regh);
+    check_exec_line(&MSEC, memory, env);
 }
